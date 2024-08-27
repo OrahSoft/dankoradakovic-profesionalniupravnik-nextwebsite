e61c4b5fe69ccf7b694f907f4644ac4b7b79124a
1978milanbabic <1978milan.babic@gmail.com>
2024-08-27 08:03:57 +0200

    POST issues

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,7 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: 'Logout successful \nNow setup your loguot logic!' });
+  if (req.method === 'POST') {
+    // Handle POST request (e.g., logout logic)
+    res.status(200).json({ message: 'Logout successful. Now setup your logout logic!' });
+  } else {
+    // Method not allowed
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 };
 
 export default handler;
