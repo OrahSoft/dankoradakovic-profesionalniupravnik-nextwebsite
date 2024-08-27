9b449c2f88668507de80fa99fcee9c5679e158f3
1978milanbabic <1978milan.babic@gmail.com>
2024-08-27 04:50:50 +0200

    testing variables

--- src/pages/api/articles.js
+++ src/pages/api/articles.js
@@ -1,6 +1,10 @@
 export default function handler(req, res) {
   res.status(200).json([
-    { id: 1, title: 'Article 1', excerpt: 'This is a short excerpt for article 1.' },
+    {
+      id: 1,
+      title: 'Article 1',
+      excerpt: `This is a short excerpt for article 1. ${process.env.TEST_VARIABLE || 'failed'}`,
+    },
     { id: 2, title: 'Article 2', excerpt: 'This is a short excerpt for article 2.' },
     { id: 3, title: 'Article 3', excerpt: 'This is a short excerpt for article 3.' },
   ]);
